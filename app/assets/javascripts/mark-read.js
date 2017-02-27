afa42e9af1a38a753e5f6d9a39e194f85e6f9c6e
Megan Talbot <meganftalbot@gmail.com>
2017-02-27 15:09:49 -0700

    Add mark as unread button and update on DOM

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -15,7 +15,6 @@ function markAsRead(e) {
     data: { read: true },
   }).then(updateStatus(linkId));
 
-
   $.ajax({
    type: "POST",
    url: "http://hot-reads-final.herokuapp.com/api/v1/reads",
@@ -29,7 +28,6 @@ function markAsRead(e) {
   });
 }
 
-
 function markAsUnRead(e) {
   e.preventDefault();
   var $link = $(this).parents('.link');
@@ -40,16 +38,23 @@ function markAsUnRead(e) {
     type: "PATCH",
     url: "/api/v1/links/" + linkId,
     data: { read: false },
-  }).then(console.log("yay"));
+  }).then(updateReadButton(linkId));
 }
 
 function updateStatus(link) {
-  $(`.link[data-link-id=${link}]`).find(".read-status").html("Read Status: true");
-  $(`.link`).addClass("marked-read")
+  $(`.link[data-link-id=${link}]`).addClass("marked-read");
+  $(`.link[data-link-id=${link}]`).find(".read-status").html("Read? true");
   updateButton(link);
 }
 
 function updateButton(link) {
-  $(`.link[data-link-id=${link}]`).find(".mark-as-read").hide();
+  $(`.link[data-link-id=${link}]`).find(".mark-as-read").remove();
   $(`.link[data-link-id=${link}]`).append('<button class="mark-as-unread">Mark as Unread</button>');
+}
+
+function updateReadButton(link) {
+  $(`.link[data-link-id=${link}]`).find(".read-status").html("Read? false");
+  $(`.link[data-link-id=${link}]`).removeClass("marked-read");
+  $(`.link[data-link-id=${link}]`).find(".mark-as-unread").remove();
+  $(`.link[data-link-id=${link}]`).append('<button class="mark-as-read">Mark as Read</button>');
 }
