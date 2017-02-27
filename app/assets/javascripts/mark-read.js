b5438dc1915aea3da892a94c1efe7c3910a75ce7
Megan Talbot <meganftalbot@gmail.com>
2017-02-27 11:46:38 -0700

    Add initial ajax call to heroku hot reads

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -12,9 +12,22 @@ function markAsRead(e) {
     type: "PATCH",
     url: "/api/v1/links/" + linkId,
     data: { read: true },
-  }).then(updateStatus);
+  }).then(updateStatus(linkId));
+
+  $.ajax({
+   type: "POST",
+   url: "http://hot-reads-final.herokuapp.com/api/v1/reads",
+   data: {link: url},
+   success: function(){
+      alert("Marked as Read on Hot Reads");
+     },
+    error: function(error){
+      console.log(error);
+    }
+  });
+
 }
 
 function updateStatus(link) {
-  $(`.link[data-link-id=${link.id}]`).find(".read-status").text(link.read);
+  $(`.link[data-link-id=${link}]`).find(".read-status").text(link.read);
 }
