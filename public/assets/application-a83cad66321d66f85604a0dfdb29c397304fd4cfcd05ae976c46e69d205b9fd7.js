ea43267cf726f9f725504158aa78ad85c95ec1c7
Megan Talbot <meganftalbot@gmail.com>
2017-02-27 18:48:27 -0700

    Precompile

--- public/assets/application-a83cad66321d66f85604a0dfdb29c397304fd4cfcd05ae976c46e69d205b9fd7.js
+++ public/assets/application-a83cad66321d66f85604a0dfdb29c397304fd4cfcd05ae976c46e69d205b9fd7.js
@@ -33241,7 +33241,7 @@ function createLink(event) {
   console.log("win");
 
   var link = getLinkData();
-  $("#notice").html('');
+  $(".message").html('');
   $.post("/api/v1/links", link).then(renderLink).fail(displayFailure);
 }
 
@@ -33268,65 +33268,67 @@ function clearLink() {
 }
 
 function displayFailure(failureData) {
-  $(".notice").html(failureData.responseText);
+  var data = failureData.responseText;
+  var message = JSON.parse(data)[0];
+  $(".message").append('<p class="error">' + message + '</p>');
   console.log("FAILED attempt to create new Link: " + failureData.responseText);
 };
-$(document).ready(function(){
-  $('#filter-by-read').on('click', filterByRead)
-  $('#filter-by-unread').on('click', filterByUnread)
-  $('#filter-links-text').on('keyup', filterText)
-  $('#show-all').on('click', showAllLinks)
-})
+'use strict';
+
+$(document).ready(function () {
+  $('#filter-by-read').on('click', filterByRead);
+  $('#filter-by-unread').on('click', filterByUnread);
+  $('#filter-links-text').on('keyup', filterText);
+  $('#show-all').on('click', showAllLinks);
+});
 
 function filterText(e) {
   e.preventDefault();
-  var filter = $( "#filter-links-text" ).val().toLowerCase();
-  $("#links-list").children().each(function(){
+  var filter = $("#filter-links-text").val().toLowerCase();
+  $("#links-list").children().each(function () {
 
     var title = $(this).find('.link-title').text();
     var url = $(this).find('.link-url').text();
 
     if (title.includes(filter) || url.includes(filter)) {
-      $(this).show()
+      $(this).show();
     } else {
-      $(this).hide()
+      $(this).hide();
     };
   });
 }
 
 function filterByUnread() {
   var read = "Read? false";
-  $('#links-list').children().each(function() {
+  $('#links-list').children().each(function () {
     var status = $(this).find(".read-status").html();
     if (status.includes(read)) {
-      $(this).show()
+      $(this).show();
     } else {
-      $(this).hide()
+      $(this).hide();
     };
   });
 }
 
 function filterByRead() {
   var read = "Read? true";
-  $('#links-list').children().each(function() {
+  $('#links-list').children().each(function () {
     var status = $(this).find(".read-status").html();
     if (status.includes(read)) {
-      $(this).show()
+      $(this).show();
     } else {
-      $(this).hide()
+      $(this).hide();
     };
   });
 }
 
 function showAllLinks() {
   var read = "Read? true";
-  $('#links-list').children().each(function() {
+  $('#links-list').children().each(function () {
 
-      $(this).show();
-
+    $(this).show();
   });
-}
-;
+};
 $( document ).ready(function(){
   $("body").on("click", ".mark-as-read", markAsRead)
   $("body").on("click", ".mark-as-unread", markAsUnRead)
